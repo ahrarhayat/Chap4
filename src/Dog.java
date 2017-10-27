a80d7691bfb24bb32e3c57312bee9c0efe5ce0cf
ahrarhayat <ahrarhayat@gmail.com>
2017-10-27 18:10:50 +0600

    6:10 update

--- src/Dog.java
+++ src/Dog.java
@@ -1,11 +1,26 @@
 public class Dog {
     String name;
-    int size;
+  private   int size ;
+
+  public int getSize(){
+
+      return size;
+    }
+    public void setSize(int s)
+    {
+        if(s>0){
+            size =s;
+        }
+        else {
+            System.out.println("Size must be greater than 0");
+        }
+
+    }
 
     public void bark(int NoOfBarks) {
         while (NoOfBarks > 0) {
             if (size > 10) {
-                System.out.println(name + " says Ruff! Ruff!");
+                System.out.println(name   + " says Ruff! Ruff!");
             } else if (size > 5) {
                 System.out.println(name + "says bark! bark!");
             } else {
--- src/DogTestDrive.java
+++ src/DogTestDrive.java
@@ -4,16 +4,16 @@ public class DogTestDrive {
     public static void main(String args[]) {
         Dog one = new Dog();
         one.name = "German Shepherd";
-        one.size = 20;
+        one.setSize(20);
         Dog two = new Dog();
         two.name = "Bull Dog";
-        two.size = 7;
+        two.setSize(10);
         Dog three = new Dog();
         three.name = "Deshi";
-        three.size = 4;
+        three.setSize(0);
         one.bark(1);
-        two.bark(0);
-        three.bark(2);
+        two.bark(1);
+        three.bark(1);
     }
 
 
